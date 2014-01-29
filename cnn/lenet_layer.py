19b7cdd3b5db2d26812f5847f53395782efcf455
IraKorshunova <i.hmarynka@gmail.com>
2014-01-29 22:26:41 +0100

    now we are new

--- cnn/lenet_layer.py
+++ cnn/lenet_layer.py
@@ -6,34 +6,32 @@ from theano import tensor as T
 
 
 class LeNetConvPoolLayer(object):
-
     def __init__(self, rng, input, filter_shape, image_shape, poolsize):
-       
         assert image_shape[1] == filter_shape[1]
-    
+
         fan_in = numpy.prod(filter_shape[1:])
         fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) / numpy.prod(poolsize))
         W_bound = numpy.sqrt(6. / (fan_in + fan_out))
-        
+
         W_values = numpy.asarray(rng.uniform(low=-W_bound, high=W_bound, size=filter_shape), dtype='float32')
         self.W = theano.shared(value=W_values, name='W', borrow=True)
 
         b_values = numpy.zeros((filter_shape[0],), dtype='float32')
         self.b = theano.shared(value=b_values, name='b', borrow=True)
-        
+
         # new code
         self.filter_shape = [filter_shape[k] for k in [0, 2, 1, 3]]
-        self.W_shuffled = self.W.dimshuffle(0, 2, 1, 3) 
-        self.W_shuffled = self.W_shuffled[:, :, ::-1, :] 
-         
-        self.input_shape = [image_shape[k] for k in [0, 2, 1, 3]] 
-        self.input_shuffled = input.dimshuffle(0, 2, 1, 3) 
-        
-        conv_out = conv.conv2d(self.input_shuffled, self.W_shuffled, filter_shape=self.filter_shape, image_shape=self.input_shape)
+        self.W_shuffled = self.W.dimshuffle(0, 2, 1, 3)
+        self.W_shuffled = self.W_shuffled[:, :, ::-1, :]
+
+        self.input_shape = [image_shape[k] for k in [0, 2, 1, 3]]
+        self.input_shuffled = input.dimshuffle(0, 2, 1, 3)
+
+        conv_out = conv.conv2d(self.input_shuffled, self.W_shuffled, filter_shape=self.filter_shape,
+                               image_shape=self.input_shape, subsample=(20, 1))
 
         pooled_out = downsample.max_pool_2d(conv_out, poolsize, ignore_border=True)
 
         self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-        #self.output = T.maximum(0,pooled_out + self.b.dimshuffle('x', 0, 'x', 'x')) relu
 
         self.params = [self.W, self.b]
--- cnn/logreg_layer.py
+++ cnn/logreg_layer.py
@@ -2,38 +2,31 @@ import numpy
 import theano
 import theano.tensor as T
 
+
 class LogisticRegressionLayer(object):
-  
     def __init__(self, input, n_in, n_out):
-        
         self.W = theano.shared(value=numpy.zeros((n_in, n_out), dtype='float32'),
-                                name='W', borrow=True)
+                               name='W', borrow=True)
         self.b = theano.shared(value=numpy.zeros((n_out,), dtype='float32'),
                                name='b', borrow=True)
-        
+
         self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
         self.y_pred = T.argmax(self.p_y_given_x, axis=1)
         self.params = [self.W, self.b]
-        
+
     def negative_log_likelihood(self, y):
         return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
-        #return -T.mean(T.log(self.p_y_given_x)[T.nonzero(y), 1])- T.mean(T.log(self.p_y_given_x)[T.nonzero(y-1), 0]) 
 
-   
-    def ber(self, y):
-        tp = T.and_(T.eq(y, 1), T.eq(self.y_pred, 1)).sum() 
-        tn = T.and_(T.eq(y, 0), T.eq(self.y_pred, 0)).sum() 
-        fp = T.and_(T.eq(y, 0), T.eq(self.y_pred, 1)).sum() 
-        fn = T.and_(T.eq(y, 1), T.eq(self.y_pred, 0)).sum()
-        return 0.5 * (T.true_div(fn, (tn + fn))  + T.true_div(fp,(tp + fp)))
-    
+    def weighted_negative_log_likelihood(self, y):
+        return -T.mean(T.log(self.p_y_given_x)[T.nonzero(y), 1]) - T.mean(T.log(self.p_y_given_x)[T.nonzero(y - 1), 0])
+
+
     def tptn(self, y):
         tp = T.and_(T.eq(y, 1), T.eq(self.y_pred, 1)).sum()
         tn = T.and_(T.eq(y, 0), T.eq(self.y_pred, 0)).sum()
-        return [tp, tn]  
-    
+        return [tp, tn]
+
     def fpfn(self, y):
         fp = T.and_(T.eq(y, 0), T.eq(self.y_pred, 1)).sum()
         fn = T.and_(T.eq(y, 1), T.eq(self.y_pred, 0)).sum()
-        return [fp, fn]  
-      
+        return [fp, fn]
