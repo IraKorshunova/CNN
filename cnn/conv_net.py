51a52de0a5a1ebb3665335b7b02bac409e620fc7
IraKorshunova <i.hmarynka@gmail.com>
2014-02-23 19:44:38 +0100

    num_check_of grad

--- cnn/conv_net.py
+++ cnn/conv_net.py
@@ -54,14 +54,16 @@ class ConvNet(object):
         self.params = self.layer3.params + layer2.params + layer1.params + layer0.params
 
     def validate(self, train_set, valid_set, init_learning_rate, learning_rate_decay, max_epochs,
-                                    max_fails, improvement_threshold, valids_per_epoch):
+                 max_fails, improvement_threshold, valids_per_epoch):
 
-        learning_rate = init_learning_rate
         train_set_iterator = TrainSetIterator(train_set)
         n_batches = train_set_iterator.get_number_of_batches()
 
         valid_set_x, valid_set_y = valid_set
         valid_size = valid_set_x.shape[0]
+
+        learning_rate_decay = np.float32(learning_rate_decay)
+        learning_rate = theano.shared(np.float32(init_learning_rate))
 
         cost = self.layer3.negative_log_likelihood(self.y)
         grads = T.grad(cost, self.params)
@@ -70,7 +72,51 @@ class ConvNet(object):
         for param_i, grad_i in zip(self.params, grads):
             updates.append((param_i, param_i - learning_rate * grad_i))
 
+        # mean_square = [theano.shared(np.zeros_like(param_i.get_value())) for param_i in self.params]
+        # for ms in mean_square:
+        #     print ms.get_value().shape
+        #
+        #
+        # updates = []
+        # for param_i, grad_i, mean_square_i in zip(self.params, grads, mean_square):
+        #     updates.append((param_i, param_i - learning_rate * grad_i / T.sqrt(0.9 * mean_square_i + 0.1 * grad_i ** 2)))
+        #     updates.append((mean_square_i, 0.9 * mean_square_i + 0.1 * grad_i ** 2))
+
         #----------- FUNCTIONS
+        check_idx = 0
+        eps = 0.001
+        orig = self.params[check_idx].get_value()
+        f_cost = theano.function([self.x, self.y, Param(self.training_mode, default=1)], cost,
+                                      on_unused_input='ignore')
+        f_grad = theano.function([self.x, self.y, Param(self.training_mode, default=1)], grads,
+                                      on_unused_input='ignore')
+
+        plus = orig
+        plus[0,0] = orig[0,0] + eps
+        self.params[check_idx].set_value(plus)
+        cost_plus = 0
+        for i in train_set_iterator:
+            cost_plus = f_cost(i[0], i[1])
+            break
+
+        minus = orig
+        minus[0,0] = orig[0,0] - eps
+        self.params[check_idx].set_value(minus)
+        cost_minus = 0
+        for i in train_set_iterator:
+            cost_minus = f_cost(i[0], i[1])
+            break
+
+        grad_num = (cost_plus - cost_minus)/ (2*eps)
+        print grad_num
+
+        self.params[check_idx].set_value(orig)
+        for i in train_set_iterator:
+            print f_grad(i[0], i[1])[check_idx]
+            break
+        return
+
+
         weighted_cost = self.layer3.weighted_negative_log_likelihood(self.y)
         ber = self.layer3.ber(self.y)
         tp, tn = self.layer3.tptn(self.y)
@@ -102,15 +148,26 @@ class ConvNet(object):
 
         while (epoch < max_epochs) and (not done_looping):
             epoch += 1
+            if epoch % 2 == 0:
+                learning_rate.set_value(max(learning_rate.get_value() / 2.0, 0.01))
+
             for batch in train_set_iterator:
                 iter += 1
+                g = f_grad(batch[0], batch[1])
+                print g[check_idx]
+                # for gg in g:
+                #     print gg.shape
+                #     print np.where(gg==0)
+                # print '================================================='
+                return
                 train_model(batch[0], batch[1])
-                learning_rate -= learning_rate_decay
+
+                # learning_rate.set_value(max(learning_rate.get_value() - learning_rate_decay, 0.01))
                 # ------------------------ VALIDATION
                 if iter % validation_frequency == 0:
                     self.batch_size.set_value(valid_size)
                     [weighted_cost, cost, tp, tn, fp, fn] = validate_model(valid_set_x, valid_set_y)
-                    print epoch, iter, tp, tn, fp, fn, cost, learning_rate
+                    print epoch, iter, tp, tn, fp, fn, cost, learning_rate.get_value()
 
                     self.batch_size.set_value(1)
 
@@ -123,7 +180,7 @@ class ConvNet(object):
                         best_iter = iter
                         best_cost = cost
 
-                    if fails >= max_fails or learning_rate < 0:
+                    if fails >= max_fails:
                         done_looping = True
                         break
 
@@ -157,7 +214,6 @@ class ConvNet(object):
         test_model = theano.function([self.x, self.y, Param(self.training_mode, default=0)], [tp, tn, fp, fn],
                                      on_unused_input='ignore')
 
-
         iter = 0
         epoch = 0
         #------------------------------  TRAINING
@@ -170,7 +226,7 @@ class ConvNet(object):
                 learning_rate -= learning_rate_decay
                 if learning_rate < 0:
                     break
-        #------------------------------  TESTING
+                    #------------------------------  TESTING
         self.batch_size.set_value(test_size)
         [tp, tn, fp, fn] = test_model(test_set_x, test_set_y)
         print 'test'
