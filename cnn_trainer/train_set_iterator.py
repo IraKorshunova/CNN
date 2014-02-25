d14c33d26319d887287120cabeda0af55bbd60c9
IraKorshunova <i.hmarynka@gmail.com>
2014-02-25 12:07:13 +0100

    check seed

--- cnn_trainer/train_set_iterator.py
+++ cnn_trainer/train_set_iterator.py
@@ -3,27 +3,41 @@ import numpy as np
 
 class TrainSetIterator(object):
 
-    def __init__(self, dataset):
-        self.x, self.y = dataset
+    def __init__(self, dataset, batch_size):
+        x, y = dataset
+        np.random.seed(42)
+        idx = np.arange(x.shape[0])
+        np.random.shuffle(idx)
+        self.x = x[idx, :]
+        self.y = y[idx]
         self.index = 0
-        self.max_index = self.x.shape[0] - 1
+        self.batch_size = batch_size
+        self.size = self.x.shape[0]
+
 
     def __iter__(self):
         return self
 
     def next(self):
-        if self.index > self.max_index:
-            self.__restart()
-            raise StopIteration
+        begin = self.index
+        end = self.index + self.batch_size
+        if end <= self.size:
+            x = self.x[begin:end, :]
+            y = self.y[begin:end]
+            self.index += self.batch_size
+            return x, y
         else:
-            x = np.transpose(self.x[self.index, :, None])
-            y = np.array([self.y[self.index,]])
-            self.index += 1
-        return x,y
+             self.__restart()
+             raise StopIteration
+
 
     def __restart(self):
+        np.random.seed(42)
+        idx = np.arange(self.size)
+        np.random.shuffle(idx)
+        self.x = self.x[idx, :]
+        self.y = self.y[idx]
         self.index = 0
 
-
     def get_number_of_batches(self):
-        return  self.max_index + 1
+        return  self.size / self.batch_size
