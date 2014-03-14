ea291a3b62420518bc9a8a8e78d839e276c40efc
IraKorshunova <i.hmarynka@gmail.com>
2014-03-14 11:28:27 +0100

    filter with arbitrary stride

--- utils/filter.py
+++ utils/filter.py
@@ -6,7 +6,9 @@ import shutil
 import os
 import re
 
+stride = 1  # in seconds
 win_len = 1000
+n_channels = 18
 ignore_after_seizure = win_len * 3 * 2
 rate = 5
 
@@ -30,8 +32,8 @@ def filter_subsample_normalize(path_in, path_out):
         x = x[::rate, ]
         y = y[::rate, ]
 
-        f_out = open(path_out +'/'+ os.path.basename(f_in.name), 'wb')
-        cPickle.dump((x,y), f_out, -1)
+        f_out = open(path_out + '/' + os.path.basename(f_in.name), 'wb')
+        cPickle.dump((x, y), f_out, -1)
         f_out.close()
 
         if i == 0:
@@ -42,13 +44,16 @@ def filter_subsample_normalize(path_in, path_out):
     mean = np.mean(all_x, 0)
     std = np.std(all_x, 0)
 
-    files = glob.glob(path_out+'/*.pickle')
+    files = glob.glob(path_out + '/*.pickle')
     for i in range(n_files):
         f = open(files[i], 'rb')
         x, y = cPickle.load(f)
         f.close()
 
         x = (x - mean) / std
+
+        x = np.float32(x)
+        y = np.int8(y)
 
         f = open(files[i], 'wb')
         cPickle.dump((x, y), f, -1)
@@ -65,17 +70,19 @@ def get_begin_end(x):
 
 
 def preprocess(path_in, path_out):
-    files = glob.glob(path_in+'/*.pickle')
+    files = glob.glob(path_in + '/*.pickle')
     n_files = len(files)
-    p = re.compile('_\d+')
+    p = re.compile('\d+')
 
     for i in range(n_files):
         print '------------------------'
-        print '===================================================file:', i
         f = open(files[i], 'rb')
         X, Y = cPickle.load(f)
-        x, y = None, None
+        f.close()
+
         be = get_begin_end(Y)
+        print '===================================================file:', os.path.basename(f.name)
+        print 'input shape:', X.shape
         print 'begin_end', be
 
         if len(be) == 1:
@@ -95,7 +102,7 @@ def preprocess(path_in, path_out):
                 y = np.concatenate((y, y2))
 
         elif len(be) > 1:
-            x, y = convert_data_cnn(X[:be[0, 0] - win_len, :])  # first seizure
+            x, y = convert_data_cnn(X[:be[0, 0] - win_len, :], y=0)  # before first seizure
             for j in range(len(be)):
                 x2, y2 = convert_data_cnn(X[be[j, 0] - win_len:be[j, 1], :], y=1)
                 x = np.concatenate((x, x2))
@@ -108,48 +115,34 @@ def preprocess(path_in, path_out):
 
                 x = np.concatenate((x, x2))
                 y = np.concatenate((y, y2))
-
         else:
             x, y = convert_data_cnn(X)
 
-        x = np.transpose(x, (0, 2, 1))
         print 'output_shape', x.shape
-        number = p.findall(os.path.basename(f.name))[0]
-        np.save(path_out + 'X'+ number, x)
-        np.save(path_out + 'Y'+ number, y)
+        number = str(int(p.findall(os.path.basename(f.name))[1]))
+        np.save(path_out + 'X_' + number, x)
+        np.save(path_out + 'Y_' + number, y)
         f.close()
 
 
 def convert_data_cnn(x, y=0):
-    if len(x) < win_len:
-        return np.ones((0, 18, win_len)), np.ones((0, 1))
-    else:
-        ns = len(x) / win_len * win_len
-        print ns
-        print 'xshape', x.shape
-
-        if len(x) > 2 * win_len:
-            x1 = reshape_data_cnn(x[:ns, :])
-            x2 = reshape_data_cnn(x[win_len / 2:ns - win_len / 2, :])
-            print x.shape, x1.shape, x2.shape
-            data_x = np.zeros((x1.shape[0]+x2.shape[0], x1.shape[1], x1.shape[2]))
-            data_x[0::2] = x1
-            data_x[1::2] = x2
-        else:
-            data_x = reshape_data_cnn(x[:ns, :])
-
+    if len(x) > win_len:
+        idx = range(0, len(x) - win_len, stride*100)
+        data_x = np.zeros((len(idx), win_len, n_channels), dtype='float32')
+        j = 0
+        for i in idx:
+            data_x[j] = x[i:i + win_len, :]
+            j += 1
         data_y = y * np.ones((len(data_x), 1))
-        return data_x, data_y
-
-def reshape_data_cnn(x):
-    n = len(x) / win_len
-    return np.transpose(np.reshape(x, (n, -1, x.shape[1])), (0, 2, 1))
+    else:
+        data_x, data_y = np.ones((0, win_len, n_channels)), np.ones((0, 1))
+    return data_x, data_y
 
 
 if __name__ == "__main__":
     patient = '08'
-    os.mkdir('../data/data'+patient+'_processed')
-    os.mkdir('../data/data'+patient+'_npy')
-    filter_subsample_normalize('../data/chb'+patient, '../data/data'+patient+'_processed')
-    preprocess('../data/data'+patient+'_processed', '../data/data'+patient+'_npy/')
-    shutil.rmtree('../data/data'+patient+'_processed')
+    os.mkdir('../data/data' + patient + '_processed')
+    os.mkdir('../data/data' + patient + '_npy')
+    filter_subsample_normalize('../data/chb' + patient, '../data/data' + patient + '_processed')
+    preprocess('../data/data' + patient + '_processed', '../data/data' + patient + '_npy/')
+    shutil.rmtree('../data/data' + patient + '_processed')
